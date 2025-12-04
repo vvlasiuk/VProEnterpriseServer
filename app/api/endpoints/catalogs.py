ff133b7865ff629a68d9d845a8d6010a235da366
Volodymyr <vlsv00@gmail.com>
2025-12-04 15:26:19 +0200

    brand

--- app/api/endpoints/catalogs.py
+++ app/api/endpoints/catalogs.py
@@ -5,13 +5,16 @@ from app.core.security import get_current_user
 
 router = APIRouter()
 
-@router.get("/catalog/{model_name}")
+# @router.get("/catalog/{model_name}")
+@router.get("/catalog")
 async def get_catalog_table(
-    model_name: str,
+    model_name: str = Query(...),
+    skip: int = Query(0, ge=0),
+    limit: int = Query(10, ge=1, le=100),
     current_user: dict = Depends(get_current_user)
 ):
-    if not model_name.startswith("cat_"):
-        return {"error": "Дозволено моделі з префіксом 'cat'"}
+    if not model_name.startswith("Cat_"):
+        return {"error": "Дозволено моделі з префіксом 'Cat'"}
     try:
         # Формуємо шлях до модуля, наприклад: app.models.models_catalog.cat_products_brands
         module_path = f"app.models.models_catalog.{model_name}"
@@ -22,8 +25,32 @@ async def get_catalog_table(
         table_name = model_class._db_head.get("table_name")
         if not table_name:
             return {"error": "У моделі не вказано table_name"}
-        query = f"SELECT * FROM {table_name}"
+        
+        # Загальна кількість записів
+        count_query = f"SELECT COUNT(*) as total FROM {table_name}"
+        count_result = await DatabaseService.execute_query(count_query)
+        total = count_result[0]["total"] if count_result else 0
+
+
+        # query = f"SELECT * FROM {table_name}"
+        query = f"SELECT _id, name, mark_deleted FROM {table_name} ORDER BY name OFFSET {skip} ROWS FETCH NEXT {limit} ROWS ONLY"
         data = await DatabaseService.execute_query(query)
-        return {table_name: data}
+
+        # cat_data = []
+        # for row in data:
+        #     cat = {
+        #         "id": row["_id"],
+        #         "name": row["name"]
+        #     }
+        #     cat_data.append(cat)
+
+        return {
+            "data": data,
+            "total": total,
+            "skip": skip,
+            "limit": limit,
+            "table_name": table_name,
+            "model_name": model_name
+         }
     except Exception as e:
         return {"error": str(e)}
